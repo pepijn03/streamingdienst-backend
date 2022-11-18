d2a0f4cea75ec26e6456ee39251a09400cd06259
pepijn03 <pepijnvanloenhout@gmail.com>
2022-11-18 21:17:32 +0100

    comment update fix

--- src/main/java/com/example/streamingdienst/repository/CommentRepo.java
+++ src/main/java/com/example/streamingdienst/repository/CommentRepo.java
@@ -7,13 +7,17 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import javax.transaction.Transactional;
 
 import java.util.List;
 
 @Repository
+@Transactional
 public interface CommentRepo extends JpaRepository<Comment,Integer> {
     @Modifying
-    @Query(value = "UPDATE `comment` SET `likes`=`likes` + :like WHERE id = :id; ",
+    @Query(value = "UPDATE `comment` SET `likes`=:like WHERE id = :id ",
             nativeQuery = true)
     void UpdateLike(@Param("id") int comment_id, @Param("like") int like_value);
+
+
 }
