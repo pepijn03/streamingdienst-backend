70e986343f336d41ef7c2787d2bc745ebdb04a30
pepijn03 <pepijnvanloenhout@gmail.com>
2022-11-29 22:36:05 +0100

    delete film now working

--- src/main/java/com/example/streamingdienst/repository/FilmRepo.java
+++ src/main/java/com/example/streamingdienst/repository/FilmRepo.java
@@ -1,14 +1,18 @@
 package com.example.streamingdienst.repository;
 
 import com.example.streamingdienst.model.Film;
+import org.hibernate.annotations.SQLUpdate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.sql.Statement;
 import java.util.List;
 
+@Transactional
 @Repository
 public interface FilmRepo extends JpaRepository<Film,Integer> {
 
@@ -20,9 +24,12 @@ public interface FilmRepo extends JpaRepository<Film,Integer> {
             nativeQuery = true)
     List<Film> FindFilmByGenre(@Param("id") String id);
 
+    @Modifying
     @Query(value = "DELETE FROM `film_genres` WHERE `film_id` = :id " ,
     nativeQuery = true)
     void DeleteGenreRefs(@Param("id") String id);
+
+
 
 
 
