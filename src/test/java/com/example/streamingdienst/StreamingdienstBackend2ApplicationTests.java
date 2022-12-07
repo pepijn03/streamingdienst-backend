606b2d793bc7b0cfc57fd94478a89a6e0e79bd62
pepijn3 <pepijnvanloenhout@gmail.com>
2022-12-07 13:36:53 +0100

    snyk pipeline update

--- src/test/java/com/example/streamingdienst/StreamingdienstBackend2ApplicationTests.java
+++ src/test/java/com/example/streamingdienst/StreamingdienstBackend2ApplicationTests.java
@@ -5,8 +5,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-@DataJpaTest
-@TestPropertySource(locations = "classpath:test.properties")
+//@DataJpaTest
+//@TestPropertySource(locations = "classpath:test.properties")
 @SpringBootTest
 class StreamingdienstBackend2ApplicationTests {
 
